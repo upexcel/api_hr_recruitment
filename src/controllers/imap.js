148626479e6953b6a59fba6ff0e749c41a059f57
shekhar <shekhar@excellencetechnologies.in>
2017-05-10 17:58:56 +0530

    fix eslint errors

--- src/controllers/imap.js
+++ src/controllers/imap.js
@@ -1,63 +1,61 @@
-import express from 'express'
-import BaseAPIController from './BaseAPIController'
-import ImapProvider from '../providers/ImapProvider'
-import db from '../db'
+import BaseAPIController from "./BaseAPIController";
+import ImapProvider from "../providers/ImapProvider";
 
 export class ImapController extends BaseAPIController {
     /* Controller for Save Imap Data  */
-  save = (req, res, next) => {
-    ImapProvider.save(this._db.Imap, req.checkBody, req.body, req.getValidationResult())
+	save = (req, res) => {
+		ImapProvider.save(this._db.Imap, req.checkBody, req.body, req.getValidationResult())
             .then((data) => {
-              this._db.Imap.create(data)
+	this._db.Imap.create(data)
                     .then(res.json.bind(res))
-                    .catch(this.handleErrorResponse.bind(null, res))
-            })
-            .catch(this.handleErrorResponse.bind(null, res))
-  }
+                    .catch(this.handleErrorResponse.bind(null, res));
+})
+            .catch(this.handleErrorResponse.bind(null, res));
+	}
 
     /* Get Imapp data using id */
-  idResult = (req, res, next, id) => {
-    this.getById(req, res, this._db.Imap, id, next)
-  }
+	idResult = (req, res, next, id) => {
+		this.getById(req, res, this._db.Imap, id, next);
+	}
 
     /* Imap data Update */
-  update = (req, res, next) => {
-    ImapProvider.save(this._db.Imap, req.checkBody, req.body, req.getValidationResult())
+	update = (req, res) => {
+		ImapProvider.save(this._db.Imap, req.checkBody, req.body, req.getValidationResult())
             .then((data) => {
-              this._db.Imap.update(data, { where: { id: req.params.id } })
+	this._db.Imap.update(data, { where: { id: req.params.id } })
                 .then((data) => {
-                  if (data[0]) {
-                    this.handleSuccessResponse(res, null)
-                  } else {
-                    this.handleErrorResponse(res, 'data not deleted')
-                  }
-                })
-                .catch(this.handleErrorResponse.bind(null, res))
-            })
-            .catch(this.handleErrorResponse.bind(null, res))
-  }
+	if (data[0]) {
+		this.handleSuccessResponse(res, null);
+	} else {
+		this.handleErrorResponse(res, "data not deleted");
+	}
+})
+                .catch(this.handleErrorResponse.bind(null, res));
+})
+            .catch(this.handleErrorResponse.bind(null, res));
+	}
 
     /* Imap data delete */
 
-  deleteImap = (req, res, next) => {
-    this._db.Imap.destroy({ where: { id: req.params.id } })
+	deleteImap = (req, res) => {
+		this._db.Imap.destroy({ where: { id: req.params.id } })
         .then((data) => {
-          if (data) {
-            this.handleSuccessResponse(res, null)
-          } else {
-            this.handleErrorResponse(res, 'data not deleted')
-          }
-        })
-        .catch(this.handleErrorResponse.bind(null, res))
-  }
+	if (data) {
+		this.handleSuccessResponse(res, null);
+	} else {
+		this.handleErrorResponse(res, "data not deleted");
+	}
+})
+        .catch(this.handleErrorResponse.bind(null, res));
+	}
 
     /* Get Imap data */
-  getImap = (req, res, next) => {
-    this._db.Imap.findAll({ offset: (req.params.page - 1) * 10, limit: 10 })
+	getImap = (req, res) => {
+		this._db.Imap.findAll({ offset: (req.params.page - 1) * 10, limit: 10 })
             .then(res.json.bind(res))
-            .catch(this.handleErrorResponse.bind(null, res))
-  }
+            .catch(this.handleErrorResponse.bind(null, res));
+	}
 }
 
-const controller = new ImapController()
-export default controller
+const controller = new ImapController();
+export default controller;
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,31 +1,27 @@
-import express from 'express'
-import BaseAPIController from './BaseAPIController'
-import UserProvider from '../providers/UserProvider.js'
-import LoginProvider from '../providers/UserProvider.js'
-import db from '../db'
-import expressJwt from 'express-jwt'
-import jwt from 'jsonwebtoken'
+import BaseAPIController from "./BaseAPIController";
+import UserProvider from "../providers/UserProvider.js";
+import LoginProvider from "../providers/UserProvider.js";
 
 export class UserController extends BaseAPIController {
     /* Controller for User Register  */
-  create = (req, res, next) => {
-    const user = UserProvider.create(this._db.User, req.checkBody, req.body, req.getValidationResult())
+	create = (req, res) => {
+		UserProvider.create(this._db.User, req.checkBody, req.body, req.getValidationResult())
             .then((user) => {
-              this._db.User.create(user)
+	this._db.User.create(user)
                     .then(res.json.bind(res))
-                    .catch(this.handleErrorResponse.bind(null, res))
-            })
-            .catch(this.handleErrorResponse.bind(null, res))
-  }
+                    .catch(this.handleErrorResponse.bind(null, res));
+})
+            .catch(this.handleErrorResponse.bind(null, res));
+	}
 
     /* Controller for User Login  */
-  login = (req, res) => {
-    let login = LoginProvider.login(this._db.User, req.body)
-    this._db.User.login(login)
+	login = (req, res) => {
+		let login = LoginProvider.login(this._db.User, req.body);
+		this._db.User.login(login)
             .then(res.json.bind(res))
-            .catch(this.handleErrorResponse.bind(null, res))
-  }
+            .catch(this.handleErrorResponse.bind(null, res));
+	}
 }
 
-const controller = new UserController()
-export default controller
+const controller = new UserController();
+export default controller;
