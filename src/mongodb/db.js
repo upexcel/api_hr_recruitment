bf06f70a943868daf566804cf0efc67696b3773e
shekhar <shekhar@excellencetechnologies.in>
2017-05-18 15:35:51 +0530

    fix

--- src/mongodb/db.js
+++ src/mongodb/db.js
@@ -1,14 +1,19 @@
 var mongoose = require("mongoose");
 var conn = mongoose.createConnection("mongodb://localhost/EMAILPANEL");
 var DB = require("../inbox");
+var CronJob = require("cron").CronJob;
 // the middleware function
 module.exports = function() {
     // create schema
 	var emailSchema = mongoose.Schema({}, {
-		collection: "emailStored",
+		collection: "emailSave",
 		strict: false,
 	});
 	var email = conn.model("EMAIL", emailSchema);
+
+	new CronJob("*/15 * * * *", function() {
+		console.log("You will see this message every second");
+	}, null, true, "America/Los_Angeles");
 	DB.get_schema(email);
 	return function(req, res, next) {
 		req.email = email;
