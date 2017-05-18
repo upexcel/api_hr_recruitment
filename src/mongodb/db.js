e851046280ce02a2488f7e5f0d2530551f9bb4d0
saurabh <saurabhk_etech@excellencetechnologies.in>
2017-05-18 10:23:32 +0530

    create fetch_all_email_details

--- src/mongodb/db.js
+++ src/mongodb/db.js
@@ -1,17 +1,17 @@
 var mongoose = require("mongoose");
 var conn = mongoose.createConnection("mongodb://localhost/EMAILPANEL");
 var DB = require("../inbox");
- // the middleware function
+// the middleware function
 module.exports = function() {
     // create schema
-	var emailSchema = mongoose.Schema({}, {
-		collection: "emailStored",
-		strict: false,
-	});
-	var email = conn.model("EMAIL", emailSchema);
-	DB.get_schema(email);
-	return function(req, res, next) {
-		req.email = email;
-		next();
-	};
+    var emailSchema = mongoose.Schema({}, {
+        collection: "emailStored",
+        strict: false,
+    });
+    var email = conn.model("EMAIL", emailSchema);
+    DB.get_schema(email);
+    return function(req, res, next) {
+        req.email = email;
+        next();
+    };
 };
