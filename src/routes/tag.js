2c7994c2ad806bd1a7bcb546fcb9936041832182
saurabh <saurabh>
2017-05-19 10:40:17 +0530

    changes in smtp api

--- src/routes/tag.js
+++ src/routes/tag.js
@@ -3,7 +3,7 @@ import auth from "../middleware/auth";
 
 export default (app) => {
     /* Route for tag save  */
-	app.route("/tag/add/:type").post( tag.save);
+	app.route("/tag/add/:type").post(auth.requiresAdminOrHr, tag.save);
 
     /* Route for tag update  */
 	app.route("/tag/update/:type/:tagId").put(auth.requiresAdminOrHr, tag.update);
@@ -14,7 +14,7 @@ export default (app) => {
     /* Route for fetch tag Data */
 	app.route("/tag/get/:type/:page").get(auth.requiresAdminOrHr, tag.getTag);
 
-    /* Route for fetch all tag data */
+		/* Route for fetch all tag data */
 	app.route("/tag/get").get(auth.requiresAdminOrHr, tag.getAllTag);
 
     /* Route for fetch tag by id */
