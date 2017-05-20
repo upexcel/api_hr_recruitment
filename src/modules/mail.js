72e56438e85cb3829dee2021c8e4a96d6ec3a41e
shekhar <shekhar@excellencetechnologies.in>
2017-05-20 13:18:39 +0530

    remove commented code

--- src/modules/mail.js
+++ src/modules/mail.js
@@ -1,10 +1,7 @@
 var nodemailer = require("nodemailer");
-// var hbs = require("nodemailer-express-handlebars");
 var smtpTransport = require("nodemailer-smtp-transport");
-// var mandrill = require("mandrill-api/mandrill");
 module.exports = {
 	mail_alert: function (email, subject, template, from, html, callback) {
-		// var mandrill_client = new mandrill.Mandrill("Ca4nS3QStEcpvZdk9iMh0Q");
 		var mailer = nodemailer.createTransport(smtpTransport({
 			host: "smtp.sendgrid.net",
 			port: 25,
