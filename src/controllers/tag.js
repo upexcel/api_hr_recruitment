49780f942c4e6dce9ef94b8acdffdeb3204122fa
shekhar <shekhar@excellencetechnologies.in>
2017-05-20 17:22:38 +0530

    fix issue in assign tag

--- src/controllers/tag.js
+++ src/controllers/tag.js
@@ -6,7 +6,6 @@ export class ImapController extends BaseAPIController {
     /* Controller for Save Imap Data  */
 	save = (req, res) => {
 		TagProvider.save(this._db, req.params.type, req.checkBody, req.body, req.getValidationResult())
-
             .then((data) => {
 	this._db.Tag.create(data)
                     .then(res.json.bind(res))
