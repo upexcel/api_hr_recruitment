57425980f194500174ffc7827373c2019eee18b0
saurabh <saurabhk_etech@excellencetechnologies.in>
2017-05-29 12:07:02 +0530

    fixed commtnts on pr 39

--- src/mongodb/db.js
+++ src/mongodb/db.js
@@ -1,21 +1,32 @@
 var mongoose = require("mongoose");
 var conn = mongoose.createConnection("mongodb://localhost/EMAILPANEL");
-var inbox = require("../inbox");
-var CronJob = require("cron").CronJob;
+import cronService from "../service/cron.js";
 // the middleware function
 module.exports = function() {
 
     // create schema
-    var emailSchema = mongoose.Schema({}, {
+    var emailSchema = mongoose.Schema({
+        email_id: { type: String },
+        from: { type: String },
+        to: { type: String },
+        sender_mail: { type: String },
+        date: { type: String },
+        email_date: { type: String },
+        email_timestamp: { type: String },
+        subject: { type: String },
+        unread: { type: Boolean },
+        answered: { type: Boolean },
+        uid: { type: Number },
+        body: { type: String },
+        tags: { type: String },
+        Genuine_Applicant: { type: String },
+        attachment: { type: String }
+    }, {
         collection: "emailStored",
-        strict: false,
+        strict: true,
     });
     var email = conn.model("EMAIL", emailSchema);
-
-    new CronJob("*/15 * * * *", function() {
-        inbox.fetch_email(email); // running this function every 15 min
-    }, null, true);
-
+    cronService.cron(email)
     return function(req, res, next) {
         req.email = email;
         next();
