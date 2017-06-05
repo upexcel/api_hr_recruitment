209c4fd4b6eeaeb27fd2c7bec5c629dbde64ea31
saurabh <saurabhk_etech@excellencetechnologies.in>
2017-06-05 12:16:56 +0530

    update

--- src/controllers/template.js
+++ src/controllers/template.js
@@ -71,8 +71,12 @@ export class TemplateController extends BaseAPIController {
     templateEmail = (req, res) => {
         TemplateProvider.templateEmail(this._db, req.checkBody, req.body, req.getValidationResult())
             .then((template) => {
-                mail.sendMail(req.params.email, template.subject, constant().smtp.text, constant().smtp.from, template.body)
-                    .then((response) => { res.json(response) })
+                this._db.Smtp.findOne({ where: { status: true } })
+                    .then((data) => {
+                        mail.sendMail(req.params.email, template.subject, constant().smtp.text, data.email, template.body)
+                            .then((response) => { res.json(response) })
+                    })
+
             })
             .catch(this.handleErrorResponse.bind(null, res));
     }
