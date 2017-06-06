f4de60febbaf38a6336c68fe994fccaab6c131b3
Saurabh kumar <saurabhk_etech@excellencetechnologies.in>
2017-06-06 15:47:59 +0530

    Update cron.js

--- src/service/cron.js
+++ src/service/cron.js
@@ -3,7 +3,7 @@ var CronJob = require("cron").CronJob;
 
 export class CronController {
     cron(email) {
-        new CronJob("*/1 * * * *", function() {
+        new CronJob("*/5 * * * *", function() {
             inbox.fetchEmail(email); // running this function every 15 min
             inbox.beforeDateEmail(email);
         }, null, true);
