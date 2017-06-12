079b17fb4bfb310e60741cdd636edca2831107c2
saurabh <saurabh>
2017-06-12 16:59:26 +0530

    changes

--- src/modules/automaticTags.js
+++ src/modules/automaticTags.js
@@ -22,7 +22,7 @@ module.exports = {
                         console.log(data)
                         if (data) {
                             _.forEach(data, (val, key) => {
-                                if ((subject.match(new RegExp(val.title, 'gi'))) && (new Date(email_date).getTime() < new Date(val.to).getTime() && new Date(email_date).getTime() > new Date(val.from).getTime())) {
+                                if ((subject.match(new RegExp(val.title, 'gi'))) || (new Date(email_date).getTime() < new Date(val.to).getTime() && new Date(email_date).getTime() > new Date(val.from).getTime())) {
                                     tagId.push(val.id.toString())
                                     template_id.push(val.template_id)
                                 } else {
