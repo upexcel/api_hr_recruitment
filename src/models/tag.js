6c5cb93559e9f31b153097a2fda6eae3798f1285
saurabh <saurabhk_etech@excellencetechnologies.in>
2017-06-19 15:52:41 +0530

    update

--- src/models/tag.js
+++ src/models/tag.js
@@ -63,7 +63,7 @@ export default function(sequelize, DataTypes) {
                             var id = []
                             console.log(tag.to)
                             _.map(data, (val, key) => {
-                                if ((val.subject.match(new RegExp(tag.title, 'gi'))) || ((new Date(val.date).getTime() < new Date(tag.to).getTime() && new Date(val.date).getTime() > new Date(tag.from).getTime())) || (val.sender_mail.match(new RegExp(tag.email, 'gi')))) {
+                                if ((val.subject.match(new RegExp(tag.title, 'gi'))) || ((tag.to && tag.from) && (new Date(val.date).getTime() < new Date(tag.to).getTime() && new Date(val.date).getTime() > new Date(tag.from).getTime())) || (val.sender_mail.match(new RegExp(tag.email, 'gi')))) {
                                     id.push(val._id);
                                     if (key == (_.size(data) - 1)) {
                                         resolve(id)
