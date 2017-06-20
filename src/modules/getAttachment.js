3912606743ed25116f076cd4ff11303c8cf92e60
saurabh <saurabhk_etech@excellencetechnologies.in>
2017-06-20 16:13:08 +0530

    update

--- src/modules/getAttachment.js
+++ src/modules/getAttachment.js
@@ -147,11 +147,13 @@ var self = module.exports = {
     },
     driveUpload: function(filepath, filename) {
         return new Promise((resolve, reject) => {
+            var folderId = config.folderid;
             var drive = google.drive({ version: 'v3', auth: oauth2Client });
             fs.readFile(filepath, (err, result) => {
                 var req = drive.files.create({
                     resource: {
                         'name': filename,
+                        parents: [folderId],
                         mimeType: mime.lookup(filepath)
                     },
                     media: {
