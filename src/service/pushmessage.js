d407159780557e6c5757c91296f514cbc4546948
nodeexcel <nodeexcel>
2017-07-26 16:44:06 +0530

    update reomve useless code

--- src/service/pushmessage.js
+++ src/service/pushmessage.js
@@ -2,27 +2,7 @@ import FCM from 'fcm-push';
 import config from '../config.js';
 let pushMessage = (device_info) => {
     return new Promise((resolve, reject) => {
-        console.log(device_info)
         resolve()
-        // let server_key = config.push_message_server_key;
-        // let fcm = new FCM(serverKey);
-        // let message = {
-        //     to: device_info.token, // required fill with device token or topics
-        //     collapse_key: 'your_collapse_key',
-        //     notification: {
-        //         title: 'Title of your push notification',
-        //         body: 'Body of your push notification'
-        //     }
-        // };
-        // fcm.send(message, function(err, response) {
-        //     if (err) {
-        //         console.log("Something has gone wrong!");
-        //         reject({ error: 1, message: err, data: [] })
-        //     } else {
-        //         console.log("Successfully sent with response: ", response);
-        //         resolve({ error: 0, message: response, data: [] })
-        //     }
-        // });
     })
 }
 
