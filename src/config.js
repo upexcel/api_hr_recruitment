fc43b792e1cea3d1de98bc5a5bad1cc8a808c443
nodeexcel <nodeexcel>
2017-08-17 11:34:26 +0530

    update config file

--- src/config.js
+++ src/config.js
@@ -1,8 +1,3 @@
 var config = require("../../config.json");
-// if (process.env.NODE_ENV == "dev") {
-//     config = require("../../dev_config.json");
-// } else {
-//     config = require("../../live_config.json");
 
-// }
 export default config;
