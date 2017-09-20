22288bb38ac218887fe42e84b6e390aa4b1ee038
nodeexcel <nodeexcel>
2017-09-20 17:21:50 +0530

    update

--- src/routes/fetchMail.js
+++ src/routes/fetchMail.js
@@ -48,7 +48,7 @@ export default (app) => {
     app.route("/get/emailStatus").put(auth.requiresAdmin, fetch_email.emailStatus)
 
     /*getting email for last inputed dates*/
-    app.route("/fetch/emails/:days").get(/*auth.requiresAdmin,*/ fetch_email.fetchByDates)
+    app.route("/fetch/emails/:days").get(auth.requiresAdmin, fetch_email.fetchByDates)
 
     /*Route for find emails by tagId*/
     app.param("tag_id", fetch_email.findByTagId)
