0c39338229d91ea9f74cf4e1614dc0e32111eb34
nodeexcel <nodeexcel>
2017-10-03 18:44:58 +0530

    update for automatic email graph

--- src/service/emaillogs.js
+++ src/service/emaillogs.js
@@ -6,12 +6,10 @@ export class EmailLogs {
         return new Promise((resolve, reject) => {
             let logs = {};
             if (data && data.status) {
-                let email_response = data.email_response.response.split(" ");
-                let time = moment.unix(email_response[3]).format("MMM DD, YYYY HH:mm");
                 if (log.emailLogs) {
-                    logs = new log.emailLogs({ email: data.email_response.envelope.to, from: data.email_response.envelope.from, time: time, user: log.user.email, subject: data.subject, body: data.body })
+                    logs = new log.emailLogs({ email: data.email_response.envelope.to, from: data.email_response.envelope.from, time: new Date(), user: log.user.email, subject: data.subject, body: data.body })
                 } else {
-                    logs = new log({ email: data.email_response.envelope.to, from: data.email_response.envelope.from, time: time, user: "By Cron", subject: data.subject, body: data.body })
+                    logs = new log({ email: data.email_response.envelope.to, from: data.email_response.envelope.from, time: new Date(), user: "By Cron", subject: data.subject, body: data.body })
                 }
                 logs.save(function(err, result) {
                     if (!err)
