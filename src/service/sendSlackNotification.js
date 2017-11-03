7ef9aaaa452eef61952b95c531c375942b2d3e1f
nodeexcel <nodeexcel>
2017-11-03 11:37:00 +0530

    added slack info on config file

--- src/service/sendSlackNotification.js
+++ src/service/sendSlackNotification.js
@@ -1,11 +1,14 @@
 let WebClient = require('@slack/client').WebClient;
 import db from "../db.js";
+import config from "../config";
 
 const slackNotification = (data) => {
     return new Promise((resolve, reject) => {
         db.Slack.findOne({ where: { status: true } }).then((slackInfo) => {
-            let web = new WebClient(slackInfo.token);
-            web.chat.postMessage(slackInfo.selected_channel, data, function(err, res) {
+            let token = slackInfo ? slackInfo.token : config.SlackToken
+            let web = new WebClient(token);
+            let channel = slackInfo ? slackInfo.selected_channel : config.ChannelId;
+            web.chat.postMessage(channel, data, function(err, res) {
                 if (err) {
                     reject(err);
                 } else {
